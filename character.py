585de4da410ff13651ab118529743fae6b74399d
desparza13 <desparzaespinosa@gmail.com>
2023-11-10 07:45:13 -0600

    Añadir docstrings a la fabrica

--- character.py
+++ character.py
@@ -5,43 +5,55 @@ import constants as const
 import animation
 
 class Character(ABC):
+    """
+    Abstract base class representing a generic character in the Pawtect the Realm game.
+    This class should be subclassed to create specific character types.
+
+    Attributes:
+        char_type (str): The type of character (e.g., 'player', 'enemy').
+        animation (Animation): An animation object managing the character's sprites and movements.
+    """
+    
     def __init__(self, x, y, mob_animation, char_type, size, stats):
+        '''
+        Initialize a new character
+        '''
         self.char_type = char_type
         self.animation = animation.Animation(mob_animation, char_type, size, x, y, stats)    
         
     def collide_with_obstacles(self, dx, dy, obstacle_tiles):
-        #first we move the character
+        '''
+        Handles character collision with obstacles
+        '''
+        #Move the character
         self.animation.rect.x += dx 
         for obstacle in obstacle_tiles:
-            #check collision in the x direction
+            #Check collision in the x direction
             if obstacle[1].colliderect(self.animation.rect):
-                if dx > 0:
+                if dx > 0: #Moving right
                     self.animation.rect.right = obstacle[1].left
-                elif dx < 0:
+                elif dx < 0: #Moving left
                     self.animation.rect.left = obstacle[1].right
-        #check collision in the y direction
+        #Check collision in the y direction
         self.animation.rect.y += dy 
         for obstacle in obstacle_tiles:
             #check collision in the y direction
             if obstacle[1].colliderect(self.animation.rect):
-                if dy > 0:
+                if dy > 0: #Moving down
                     self.animation.rect.bottom = obstacle[1].top
-                elif dy < 0:
+                elif dy < 0: #Moving up
                     self.animation.rect.top = obstacle[1].bottom
                     
     def control_diagonal_speed(self,dx,dy):
+        '''
+        Adjusts the character's speed when moving diagonally to maintain a consistent overall velocity.
+        '''
         factor = (math.sqrt(2)/2)
         return (dx * factor), (dy * factor)
             
     @abstractmethod        
     def move():
+        '''
+        Abstract method to be implemented by subclasses to define character movement.
+        '''
         pass
-    
-            
-        
-
-    
-    
-            
-        
-  
--- stats.py
+++ stats.py
@@ -3,7 +3,22 @@ import pygame
 import constants as const
 
 class Stats():
+    """
+    A class to maintain the status and statistics of a game character.
+
+    Attributes:
+        stunned (bool): Flag indicating whether the character is stunned.
+        running (bool): Flag indicating whether the character is running.
+        hit (bool): Flag indicating whether the character has recently been hit.
+        alive (bool): Flag indicating whether the character is alive.
+        health (int): Current health of the character.
+        last_hit (int): Timestamp of the last time the character was hit.
+        last_attack (int): Timestamp of the last time the character attacked.
+    """
     def __init__(self, health):
+        '''
+        Initializes the Stats object with the given health value.
+        '''
         #Status
         self.stunned = False
         self.running = False
@@ -13,7 +28,4 @@ class Stats():
         #Times
         self.last_hit = pygame.time.get_ticks()
         self.last_attack = pygame.time.get_ticks()
-        
-    
-        
-        
+
