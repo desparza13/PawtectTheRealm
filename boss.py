1e61892c4c1d4c48b9e27aad2ea461b705439632
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2023-11-19 14:00:12 -0600

    refactorizar clase boss

--- boss.py
+++ boss.py
@@ -80,29 +80,66 @@ class Boss(Character):
         Defines the AI logic for the boss, including moving towards the player and attacking.
         """
         self.manage_boss_music(screen_scroll)
-
-        clipped_line = ()
         stun_cooldown = 0
         ballattack = None
         ai_dx = 0
         ai_dy = 0
         
         # Reposition the boss based on screen scroll to keep it in the correct world position
+        self.update_position_with_screen_scroll(screen_scroll)
+
+        # Determine if the boss has a clear line of sight to the player
+        line_of_sight = self.create_line_of_sight(player)
+        visible_to_boss = self.is_player_visible(obstacle_tiles,line_of_sight)
+
+        # Calculate distance to the player to determine if the boss should move or attack
+        dist = self.calculate_distance_to_player(player)
+        
+        # Determine boss's movement direction based on player's position if in range
+        should_move_towards_player = visible_to_boss and dist > const.RANGE
+        ai_dx, ai_dy = self.determine_ai_movement(player, should_move_towards_player)
+        
+        # Execute boss actions if alive and not stunned
+        ballattack = self.execute_ai_actions(ai_dx,ai_dy,obstacle_tiles, player, dist, ballattack_image, stun_cooldown)
+                
+        # Return a BallAttack object if an attack was initiated, None otherwise
+        return ballattack
+    
+    def update_position_with_screen_scroll(self, screen_scroll):
+        """
+        Updates the boss's position based on the screen scroll.
+        """
         self.animation.rect.x += screen_scroll[0]
         self.animation.rect.y += screen_scroll[1]
 
-        # Determine if the boss has a clear line of sight to the player
-        line_of_sight = ((self.animation.rect.centerx, self.animation.rect.centery), (player.animation.rect.centerx, player.animation.rect.centery))
+    def create_line_of_sight(self, player):
+        """
+        Creates a line of sight from the boss to the player for visibility checks.
+        """
+        return ((self.animation.rect.centerx, self.animation.rect.centery), (player.animation.rect.centerx, player.animation.rect.centery))
+    
+    def is_player_visible(self, obstacle_tiles, line_of_sight):
+        """
+        Checks if the player is visible to the boss, considering obstacles.
+        """
         for obstacle in obstacle_tiles:
             if obstacle[1].clipline(line_of_sight):
-                # Line of sight is blocked by an obstacle
-                clipped_line = obstacle[1].clipline(line_of_sight)
-
-        # Calculate distance to the player to determine if the boss should move or attack
-        dist = math.sqrt((self.animation.rect.centerx - player.animation.rect.centerx)**2 + ((self.animation.rect.centery - player.animation.rect.centery)**2))
+                 # Line of sight is blocked by an obstacle
+                return False
+        return True
+    
+    def calculate_distance_to_player(self, player):
+        """
+        Calculates the distance from the boss to the player.
+        """
+        return math.sqrt((self.animation.rect.centerx - player.animation.rect.centerx)**2 + 
+                         ((self.animation.rect.centery - player.animation.rect.centery)**2))
         
-        # Determine boss's movement direction based on player's position if in range
-        should_move_towards_player = not clipped_line and dist > const.RANGE
+    def determine_ai_movement(self, player,should_move_towards_player):
+        """
+        Determines the movement direction for the AI based on whether the player is visible and in range.
+        """
+        ai_dx, ai_dy = 0, 0
         if should_move_towards_player:
             if self.animation.rect.centerx > player.animation.rect.centerx:
                 ai_dx = -const.ENEMY_SPEED
@@ -112,8 +149,36 @@ class Boss(Character):
                 ai_dy = -const.ENEMY_SPEED
             if self.animation.rect.centery < player.animation.rect.centery:
                 ai_dy = const.ENEMY_SPEED
-        
-        # Execute boss actions if alive and not stunned
+        return ai_dx, ai_dy
+    
+    def attack_with_ball(self,dist,ballattack_image, ballattack_cooldown,player):
+        ''' 
+        Handles the boss's ball attack
+        '''
+        ballattack = None
+        if dist < 500:
+            if pygame.time.get_ticks() - self.animation.stats.last_attack >= ballattack_cooldown:
+                ballattack = weapon.BallAttack(ballattack_image, self.animation.rect.centerx, self.animation.rect.centery, player.animation.rect.centerx, player.animation.rect.centery)
+                self.animation.stats.last_attack = pygame.time.get_ticks()
+                
+        return ballattack
+    
+    def handle_hit_stun(self):
+        """
+        Handles the boss being hit and potentially stunned.
+        """
+        if self.animation.stats.hit:
+            self.animation.stats.hit = False
+            self.animation.stats.last_hit = pygame.time.get_ticks()
+            self.animation.stats.stunned = True
+            self.animation.stats.running = False
+            self.animation.set_action(0) #0: idle
+    
+    def execute_ai_actions(self, ai_dx, ai_dy, obstacle_tiles, player, dist, ballattack_image, stun_cooldown):
+        """
+        Executes the actions determined by the AI logic, including moving and attacking the player.
+        """
+        ballattack = None
         if self.animation.stats.alive: 
             if not self.animation.stats.stunned:
                 # Move towards the player and attempt to attack
@@ -122,26 +187,16 @@ class Boss(Character):
                 
                 # Initiate a projectile attack if within range and cooldown has passed
                 ballattack_cooldown = 700
-                if dist < 500:
-                    if pygame.time.get_ticks() - self.animation.stats.last_attack >= ballattack_cooldown:
-                        ballattack = weapon.BallAttack(ballattack_image, self.animation.rect.centerx, self.animation.rect.centery, player.animation.rect.centerx, player.animation.rect.centery)
-                        self.animation.stats.last_attack = pygame.time.get_ticks()
-
+                ballattack = self.attack_with_ball(dist, ballattack_image, ballattack_cooldown, player)
+                
             # Reset boss stats if hit
-            if self.animation.stats.hit:
-                self.animation.stats.hit = False
-                self.animation.stats.last_hit = pygame.time.get_ticks()
-                self.animation.stats.stunned = True
-                self.animation.stats.running = False
-                self.animation.set_action(0) #0: idle
+            self.handle_hit_stun()
                 
             if (pygame.time.get_ticks() - self.animation.stats.last_hit) > stun_cooldown:
                 self.animation.stats.stunned = False
-                
-        # Return a BallAttack object if an attack was initiated, None otherwise
+        
         return ballattack
-
-
+            
     def attack_the_player(self, dist, player):
         """
         Initiates an attack on the player if within a certain range.
